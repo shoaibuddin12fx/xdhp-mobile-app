e6ee65f3799a13c8e840d00c1bb27283849ff44c
mohammad-alizia <58190992+mohammad-alizia@users.noreply.github.com>
2022-11-30 17:00:17 +0500

    social media (comment, reply comment, like)

--- app/components/CommentsComp.js
+++ app/components/CommentsComp.js
@@ -1,152 +1,139 @@
-import {View} from 'native-base';
-import React, {useEffect} from 'react';
-import {useState} from 'react';
-import {StyleSheet} from 'react-native';
-import {Image} from 'react-native';
-import {Text} from 'react-native';
-import {TextInput} from 'react-native';
-import {ServiceApi} from '../Api/ServiceApi';
-import {AppColor} from '../shared/appColors';
+import { View } from 'native-base';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Image, Pressable } from 'react-native';
+import { Text } from 'react-native';
+import { TextInput } from 'react-native';
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import { ServiceApi } from '../Api/ServiceApi';
+import { AppColor } from '../shared/appColors';
 
-const CommentsComp = ({comment}) => {
-  const [reply, setReply] = useState(true);
-  const [sendReply, setSendReply] = useState({});
-  const serviceApi = new ServiceApi();
-  // var hoursDifference = Math.floor(difference/1000/60/60);
-  // difference -= hoursDifference*1000*60*60
+const serviceApi = new ServiceApi();
+const CommentsComp = ({ comment, setComments }) => {
+  // const a = require('../Assets/lady.png');
 
-  //   {
-  //     "description": "test 3 completed",
-  //     "post_id": 45,
-  //     "reply_to": 42,
-  //     "parent_comment_id": 17
-  // }
+  const [reply, setReply] = useState(false);
+  const [sendReply, setSendReply] = useState('');
 
   const handleReplies = async () => {
-    const response = await serviceApi.writeComment(sendReply);
-    setSendReply({});
-    console.log('response', response);
+    // const response = await serviceApi.writeComment(sendReply);
+    // setComments(comment.childComments[reply, ...comment.childComments])
+    if (sendReply != '' && sendReply != null) {
+
+      const dummySendReply = {
+        id: Math.random(),
+        name: 'New Commnet',
+        description: sendReply,
+        reply_to: comment.id,
+        parent_comment_id: comment.id,
+        reply: true,
+        likes: Math.floor(Math.random() * 10),
+      };
+      console.log('dummySendReply:', { dummySendReply }, { comment });
+
+      comment.childComments = [dummySendReply, ...comment.childComments];
+      console.log({ 'handle replies': comment });
+      // setComments(comment);
+      setSendReply('');
+      // console.log('response', response);}
+
+    }
+    else {
+      console.log("HANDLE REPLIES DOESNT HANDLE EMPTY COMMENTS")
+    }
+
   };
+
+  const handleLikes = async (data) => {
+    const likeResposne = await serviceApi.likePostOrComment(data);
+    console.log("likeResposne",{likeResposne})
+    // console.log("likeResposne", { data })
+  }
 
   useEffect(() => {
     console.log('reply: ', reply);
   }, [setReply]);
   useEffect(() => {
     console.log('send reply: ', sendReply);
-  }, [setSendReply]);
+  }, [setSendReply, handleReplies]);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.formRow}>
-        <View style={styles.formIcon}>
-          <View style={[styles.imageView]}>
-            <Image
-              source={require('../Assets/watch2.png')}
-              style={[styles.image]}
-            />
+    <>
+      <View style={styles.container}>
+        <View style={styles.formRow}>
+          <View style={styles.formIcon}>
+            <View style={[styles.imageView]}>
+              <Image
+                source={require('../Assets/watch2.png')}
+                style={[styles.image]}
+              />
+            </View>
           </View>
-        </View>
-        <View style={[styles.formItem]}>
-          <View style={[styles.name]}>
-            <Text style={[styles.text, {fontWeight: '700'}]}>
-              {comment.name}
+          <View style={[styles.formItem]}>
+            <View style={[styles.name]}>
+              <Text style={[styles.text, { fontWeight: '700' }]}>
+                {comment.user_id}
+              </Text>
+              <Text style={[{ color: 'lightgrey' }]}>3h</Text>
+            </View>
+            <Text style={[styles.text, { marginTop: 10 }]}>
+              {comment.description}
             </Text>
-            <Text style={[{color: 'lightgrey'}]}>3h</Text>
-          </View>
-          <Text style={[styles.text, {marginTop: 10}]}>
-            {comment.description}
-          </Text>
-          <View style={[styles.like]}>
-            <View>
-              <View style={[{flexDirection: 'row'}]}>
-                <Text style={[{color: 'lightgrey'}]}>Like</Text>
+            <View style={[styles.like]}>
+              <View>
+                <View style={[{ flexDirection: 'row' }]}>
+                  <Pressable style={[{ color: 'lightgrey' }]} onPress={() => handleLikes({ post_id: comment.post_id, comment_id: comment.comment_id })}>
+                    <Text>
+                      Like {comment.likes}
+                    </Text>
+                  </Pressable >
+                  <Pressable onPress={() => setReply(!reply)} style={[{ color: 'lightgrey', marginLeft: 10 }]}>
+                    <Text >
+                      Reply
+                    </Text>
+                  </Pressable>
+                </View>
+                <View style={[replyTo.con, { width: 230, borderRadius: 100 }]}>
+                  {reply == true && (
+                    <>
+                      <ReplyTo sendReply={sendReply} setSendReply={setSendReply} />
+                      <Text
+                        onPress={() => handleReplies()}
+                        style={[{ color: 'black' }]}>
+                        Send
+                      </Text>
+                    </>
+                  )}
+                </View>
+              </View>
+              <View>
                 <Text
-                  style={[{color: 'lightgrey', marginLeft: 10}]}
-                  onPress={() => setReply(!reply)}>
-                  Reply
+                  style={[
+                    { color: 'lightgrey', fontSize: 16, fontWeight: '700' },
+                  ]}>
+                  ...
                 </Text>
               </View>
-              <View style={[replyTo.con, {width: 230, borderRadius: 100}]}>
-                {reply == true && (
-                  <>
-                    <ReplyTo setSendReply={setSendReply} comment={comment} />
-                    <Text
-                      onPress={() => handleReplies}
-                      style={[{color: 'black'}]}>
-                      Send
-                    </Text>
-                  </>
-                )}
-              </View>
-            </View>
-            <View>
-              <Text
-                style={[{color: 'lightgrey', fontSize: 16, fontWeight: '700'}]}>
-                ...
-              </Text>
             </View>
           </View>
-          {comment.childComments.map((c, i) => {
-            return <Reply comment={c} />;
+        </View>
+        {comment.childComments.length > 0 &&
+          comment.childComments?.map((c, i) => {
+            return (
+              <View style={[{ marginLeft: 50 }]}>
+                <Reply comment={c} key={i} />
+              </View>
+            );
           })}
-        </View>
       </View>
-    </View>
+    </>
   );
 };
 
-export const Reply = ({comment}) => {
-  // const [reply, setReply] = useState(true);
 
-  return (
-    <View style={[styles.container, {marginLeft: 0, padding: 0}]}>
-      <View style={styles.formRow}>
-        <View style={styles.formIcon}>
-          <View style={[styles.imageView]}>
-            <Image
-              source={require('../Assets/watch2.png')}
-              style={[styles.image]}
-            />
-          </View>
-        </View>
-        <View style={[styles.formItem]}>
-          <View style={[styles.name]}>
-            <Text style={[styles.text, {fontWeight: '700'}]}>
-              {comment.name}
-            </Text>
-            <Text style={[{color: 'lightgrey'}]}>3h</Text>
-          </View>
-          <Text style={[styles.text, {marginTop: 10}]}>
-            {comment.description}
-          </Text>
-          <View style={[styles.like]}>
-            <View>
-              <View style={[{flexDirection: 'row'}]}>
-                <Text style={[{color: 'lightgrey'}]}>Like</Text>
-                {/* <Text
-                  style={[{color: 'lightgrey', marginLeft: 10}]}
-                  onPress={() => setReply(!reply)}>
-                  Reply
-                </Text> */}
-              </View>
-              {/* <View style={[replyTo.con, {width: 150, borderRadius: 100}]}>
-                {reply == true && <ReplyTo />}
-              </View> */}
-            </View>
-            <View>
-              <Text
-                style={[{color: 'lightgrey', fontSize: 16, fontWeight: '700'}]}>
-                ...
-              </Text>
-            </View>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
-};
 
-const ReplyTo = ({setSendReply, comment}) => {
+const ReplyTo = ({ sendReply, setSendReply }) => {
   return (
     <View
       style={[
@@ -159,34 +146,90 @@ const ReplyTo = ({setSendReply, comment}) => {
           width: '100%',
         },
       ]}>
-      {/* <Text style={{backgroundColor: 'black'}}>please kashif</Text> */}
       <TextInput
+        value={sendReply}
         placeholder="Reply"
         placeholderTextColor={AppColor.lightGray}
-        style={[{color: AppColor.dark}]}
-        onChangeText={text =>
-          setSendReply({
-            description: text,
-            post_id: comment.post_id,
-            reply_to: comment.reply_to,
-            parent_comment_id: comment.parent_comment_id,
-          })
-        }></TextInput>
+        style={[{ color: AppColor.dark }]}
+        onChangeText={text => {
+          console.log(text.length)
+          if (text == '' && text == null) {
+
+            console.log("EMPTY COMMENT")
+          }
+          else {
+            setSendReply(text)
+          }
+        }
+        }
+      ></TextInput>
     </View>
   );
 };
 
+
+export const Reply = ({ comment }) => {
+  // const [reply, setReply] = useState(true);
+  const handleLikes = async (data) => {
+    const likeResposne = await serviceApi.likePostOrComment(data);
+    console.log("likeResposne",{likeResposne})
+    // console.log("likeResposne", { data })
+  }
+  return (
+    <View style={[styles.container, { marginLeft: 0, padding: 0 }]}>
+      <View style={styles.formRow}>
+        <View style={styles.formIcon}>
+          <View style={[styles.imageView]}>
+            <Image
+              source={require('../Assets/watch2.png')}
+              style={[styles.image]}
+            />
+          </View>
+        </View>
+        <View style={[styles.formItem]}>
+          <View style={[styles.name]}>
+            <Text style={[styles.text, { fontWeight: '700' }]}>
+              {/* {comment.name} */}
+              {comment.user_id}
+            </Text>
+            <Text style={[{ color: 'lightgrey' }]}>3h</Text>
+          </View>
+          <Text style={[styles.text, { marginTop: 10 }]}>
+            {comment.description}
+          </Text>
+          <View style={[styles.like]}>
+            <View>
+              <View style={[{ flexDirection: 'row' }]}>
+              <Pressable style={[{ color: 'lightgrey' }]} onPress={() => handleLikes({ post_id: comment.post_id, comment_id: comment.comment_id })}>
+                    <Text>
+                      Like {comment.likes}
+                    </Text>
+                  </Pressable >
+              </View>
+            </View>
+            <View>
+              <Text
+                style={[{ color: 'lightgrey', fontSize: 16, fontWeight: '700' }]}>
+                ...
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40,
+    paddingTop: 20,
   },
   formRow: {
     flexDirection: 'row',
     height: 'auto',
   },
   formItem: {
-    width: '75%',
+    width: '85%',
     flex: 0,
     backgroundColor: '#F2F2F2',
     borderBottomLeftRadius: 10,
--- app/screens/Comments.js
+++ app/screens/Comments.js
@@ -3,95 +3,176 @@ import React from 'react';
 import {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {Image} from 'react-native';
-import {Text} from 'react-native';
+import {Text, TextInput} from 'react-native';
 import CommentsComp, {Reply} from '../components/CommentsComp';
 import {AppColor} from '../shared/appColors';
 import {ServiceApi} from '../Api/ServiceApi';
 import {useEffect} from 'react';
-// const comments = [
-//   {
-//     id: 1,
-//     name: 'Henry Cavil',
-//     comment: 'thats badass!',
-//     reply: false,
-//   },
-//   {
-//     id: 2,
-//     name: 'Millie Bobby Brown',
-//     comment: 'Thanks Henry ❤ 2',
-//     reply_to: 1,
-//     parent_comment_id: 1,
-//     reply: true,
-//   },
-//   {
-//     id: 3,
-//     name: 'Millie Bobby Brown',
-//     comment: 'Thanks Henry ❤ 3',
-//     reply_to: 2,
-//     parent_comment_id: 1,
-//     reply: true,
-//   },
-//   {
-//     id: 4,
-//     name: 'Millie Bobby Brown',
-//     comment: 'Thanks Henry ❤ 4',
-//     // reply_to: 1,
-//     // parent_comment_id: 1,
-//     reply: false,
-//   },
-//   {
-//     id: 5,
-//     name: 'Henry Cavil',
-//     comment: 'Hail Anolna 😂1',
-//     reply: false,
-//   },
-//   {
-//     id: 6,
-//     name: 'Millie Bobby Brown',
-//     comment: 'henryyyyyy 😂',
-//     reply_to: 5,
-//     parent_comment_id: 5,
-//     reply: true,
-//   },
-// ];
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Comments = () => {
-  console.log('these are the comments');
+  const comments = [
+    {
+      id: 1,
+      name: 'Henry Cavil',
+      description: 'thats badass!',
+      reply: false,
+      likes: 100,
+      childComments: [
+        {
+          id: 2,
+          name: 'Millie Bobby Brown',
+          description: 'Thanks Henry ❤ 2',
+          reply_to: 1,
+          parent_comment_id: 1,
+          reply: true,
+          likes: 5,
+        },
+        {
+          id: 3,
+          name: 'Millie Bobby Brown',
+          description: 'Thanks Henry ❤ 3',
+          reply_to: 2,
+          parent_comment_id: 1,
+          reply: true,
+          likes: 2,
+        },
+        {
+          id: 4,
+          name: 'Millie Bobby Brown',
+          description: 'Thanks Henry ❤ 4',
+          // reply_to: 1,
+          parent_comment_id: 1,
+          reply: false,
+          likes: 10,
+        },
+      ],
+    },
+
+    {
+      id: 5,
+      name: 'Henry Cavil',
+      description: 'Hail Anolna 😂1',
+      reply: false,
+      likes: 20,
+      childComments: [
+        {
+          id: 6,
+          name: 'Millie Bobby Brown',
+          description: 'henryyyyyy 😂',
+          reply_to: 5,
+          parent_comment_id: 5,
+          reply: true,
+          likes: 50,
+        },
+      ],
+    },
+  ];
+
+  // const a = require('../Assets/lady.png');
+  var a 
   const [comment, setComments] = useState([]);
-  const commentsAndTheirReplies = [];
+  const [writeComment, setWriteComment] = useState('');
+  // const commentsAndTheirReplies = [];
   const serviceApi = new ServiceApi();
 
   const commentsApi = async () => {
     const allComments = await serviceApi.allComments(13);
+    console.log("All Comments",allComments.data)
     setComments(allComments.data);
-    console.log('\n\n\n\n\n\n\n\n these are the comments', allComments);
-    console.log(
-      '\n\n\n\n\n\n\n\n these are the comments data',
-      allComments.data,
-    );
-    // console.log('\n\n\n\n\n\n\n\n these are the comments', allComments.childCo);
+  };
+
+  const handleSendComment = async () => {
+    // const resposne = await serviceApi.writeComment({
+    //   description: writeComment,
+    //   post_id: comment.post_id,
+    //   reply_to: comment.reply_to,
+    //   parent_comment_id: comment.parent_comment_id,
+    // });
+    let dummyComment = {
+      id: Math.random(),
+      user_id: 'New Comment' + Math.floor(Math.random() * 10),
+      description: writeComment,
+      reply: false,
+      likes: Math.floor(Math.random() * 10),
+      childComments: [],
+    };
+    setComments([dummyComment, ...comment]);
+    // Emptying the comment just written
+    setWriteComment('');
+    // send the new parent comment to the server
+    let writeComment_response = await serviceApi.writeComment(comment)
+    console.log("Write Comment Response", writeComment_response)
+    await commentsApi();
+    console.log({resposne});
   };
 
   useEffect(() => {
-    commentsApi();
-  }, []);
+    console.log({writeComment});
+  }, [setWriteComment]);
 
-  //   for (let i = 0; i < comments.length; i++) {
-  //     for (let j = 0; j < comments.length; j++) {
-  //       if (comment[i].id == comments[j].reply_to) {
-  //         console.log('\n\n\n\n\n\n\n\n\n', {
-  //           comment: comments[i].id,
-  //           reply: comments[j].id + '' + comments[j].reply_to,
-  //         });
-  //       }
-  //     }
-  //   }
+  useEffect(() => {
+    commentsApi();
+    // setComments([comment, ...comments]);
+    // setComments(comments);
+    // console.log("Comments.js", comment)
+  }, []);
 
   return (
     <>
       {comment?.map((c, i) => {
-        return <>{<CommentsComp comment={c} />}</>;
+        return <>{<CommentsComp comment={c} setComments={setComments} key={i}/>}</>;
       })}
+      <View
+        style={[
+          styles.writeCommentView,
+          {
+            height: 'auto',
+            borderTopWidth: 1,
+            paddingTop: 10,
+            // paddingBottom: 10,
+            borderTopColor: AppColor.lightGray,
+          },
+        ]}>
+        <View style={[styles.imageView, {borderRadius: 9999999}]}>
+          <Image source={a} style={[{height: 40, width: 40}]} />
+        </View>
+        <View
+          style={[
+            {
+              height: 'auto',
+              width: '80%',
+              // backgroundColor: 'blue',
+            },
+          ]}>
+          <TextInput
+            value={writeComment}
+            onChangeText={text => {
+              console.log({text});
+              setWriteComment(text);
+            }}
+            placeholder="Write a Comment"
+            style={[
+              {
+                height: 'auto',
+                width: '100%',
+                marginLeft: 5,
+                backgroundColor: 'white',
+              },
+            ]}></TextInput>
+        </View>
+        <View>
+          <Icon
+            name="ios-send"
+            size={30}
+            color={AppColor.greenButton}
+            onPress={() => {
+              handleSendComment();
+              console.log('send the comment');
+            }}
+          />
+        </View>
+      </View>
     </>
   );
 };
@@ -146,6 +227,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 10,
+  },
+  writeCommentView: {
+    // backgroundColor: 'red',
+    height: 50,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // padding: 10,
   },
 });
 export default Comments;
